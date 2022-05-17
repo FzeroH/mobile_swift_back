ca076ea9d310f5f727293ff8181ff71279f37170
FzeroH <vampir666x@gmail.com>
2022-05-17 23:33:20 +0300

    feat(UserController): add functions getUsers and createUser

---
+++ Controllers/UserController.js
@@ -0,0 +1,38 @@
+const {json} = require("express");
+const pgp = require("pg-promise")();
+const db = pgp(`postgres://${ process.env.DB_LOGIN }:${ process.env.DB_PASSWORD }@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`);
+
+
+class UserController {
+    async getUsers(res) {
+        await db.query("SELECT * FROM users")
+            .then((data) => {
+                res.send(data)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    };
+
+    async createUsers (req, res) {
+        const userData = req.body
+        await db.query(
+            'INSERT INTO users (user_name, user_login, user_password) VALUES (${username}, ${login}, ${password})',
+            {
+                username: userData.username,
+                login: userData.login,
+                password: userData.password
+            })
+            .then((data) => {
+                console.log(data)
+                res.send('OK')
+            })
+            .catch((error) => {
+                console.error(error)
+                res.send(error)
+            })
+        console.log(userData)
+    }
+}
+
+module.exports = { UserController }
