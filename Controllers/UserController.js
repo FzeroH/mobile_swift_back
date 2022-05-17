51b7ea498996309680050ac6dd429883feeec374
FzeroH <vampir666x@gmail.com>
2022-05-17 23:42:42 +0300

    refactor(UserController): delete unused variables and brought out a connection to the db

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,6 +1,4 @@
-const {json} = require("express");
-const pgp = require("pg-promise")();
-const db = pgp(`postgres://${ process.env.DB_LOGIN }:${ process.env.DB_PASSWORD }@${ process.env.DB_HOST }:${ process.env.DB_PORT }/${ process.env.DB_NAME }`);
+const { db } = require('../database')
 
 
 class UserController {
