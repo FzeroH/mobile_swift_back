0708bfbad809ec62c94f956485c87d6a4e51b88a
FzeroH <vampir666x@gmail.com>
2022-05-18 02:37:50 +0300

    feat(app.js): add create_article, get_articles_list and get_article routes

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const { UserController } = require("./Controllers/UserController");
 const app = express();
 const bodyParser = require('body-parser')
+const { ArticlesController } = require("./Controllers/ArticlesController");
 const userController = new UserController();
+const articleController = new ArticlesController();
 app.use(bodyParser.json());
 
 
@@ -17,6 +19,15 @@ app.post('/api/create_user' ,(req, res) => {
 
 app.post('/api/get_user', (req, res) => {
     userController.getUserInfo(req, res)
+});
+app.post('/api/create_article', (req, res) => {
+    articleController.createArticle(req, res)
+});
+app.get('/api/get_articles_list', (req, res) => {
+    articleController.getArticlesList(res)
+});
+app.post('/api/get_article', (req, res) => {
+    articleController.getArticle(req, res)
 });
 
 app.listen(process.env.PORT, () => {
