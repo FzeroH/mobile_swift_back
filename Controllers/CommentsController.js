022af1f97c47b84b5f52d9ff04476d26b9dc3d12
FzeroH <vampir666x@gmail.com>
2022-05-30 19:20:34 +0300

    feat(CommentsController.js): modify database request for comment list

--- Controllers/CommentsController.js
+++ Controllers/CommentsController.js
@@ -2,7 +2,7 @@ const { db } = require('../database')
 
 class CommentsController {
     async createComment (req, res) {
-        const date = new Date()
+        const date = new Date().toLocaleDateString()
         await db.query('INSERT INTO comment (blog_id, user_id, comment_text, comment_datetime) VALUES (${ blog_id }, ${ user_id }, ${ comment_text }, ${ comment_datetime })',
             {
                 blog_id: req.body.blog_id,
@@ -18,11 +18,11 @@ class CommentsController {
             })
     }
     async getListComments (req, res) {
-        await db.query('SELECT * FROM comment WHERE blog_id = ${ blog_id }', {
+        await db.query('SELECT cm.comment_id, cm.blog_id, cm.user_id, cm.comment_text, cm.comment_datetime, blog.blog_id, users.user_id, users.user_name FROM comment as cm JOIN blog ON cm.blog_id = blog.blog_id JOIN users ON cm.user_id = users.user_id WHERE cm.blog_id = ${ blog_id }', {
             blog_id: req.body.blog_id
         })
             .then((data) => {
-            res.send(data[0])
+            res.send(data)
         })
             .catch((error) => {
                 console.error(error)
